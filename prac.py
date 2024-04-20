5d2b50c4c7851042db482139f41e9d98bd9bd41d
daicaduy <vanduycn2@gmail.com>
2024-04-20 12:54:45 +0300

    first commit

---
+++ prac.py
@@ -0,0 +1,25 @@
+
+# viết mã tạo ứng dụng android đơn giản, có 1 nút, khi click vào nút sẽ gửi request và nhận response từ server https://spb-transport.gate.petersburg.ru/
+
+# Path: app.py
+import requests
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+class MyApp(App):
+    def build(self):
+        layout = GridLayout(cols=1)
+        button = Button(text='Click me')
+        button.bind(on_press=self.on_press)
+        layout.add_widget(button)
+        return layout
+
+    def on_press(self, instance):
+        url = 'https://spb-transport.gate.petersburg.ru/api/stops'
+        response = requests.get(url)
+        print(response.text)
+
+if __name__ == '__main__':
+    MyApp().run()
+
